6c8f7ec52e1bf2d106a43dc73ca15851f3a67e07
alikusnadie <alikusnadie@gmail.com>
2023-12-25 15:56:15 +0700

    add akses

--- src/lib/aksesdb.nim
+++ src/lib/aksesdb.nim
@@ -1,22 +1,26 @@
 import json
 import flatdb
 
-var dbs = newFlatDb("testdb.db", false)
+var store = newFlatDb("store.json", false)
 var userDb = newFlatDb("user.json", false)
+var trxDb = newFlatDb("trx.json", false)
+var member = newFlatDb("trx.json", false)
+var produk = newFlatDb("trx.json", false)
 
-discard dbs.load()
+discard store.load()
 discard userDb.load()
+discard trxDb.load()
 
 # Your JSON data as a string
 
 proc getQuery*(q1: string, q2: string): seq[JsonNode] = 
-  let queryd = dbs.queryDb(findDb(q1, q2))
+  let queryd = store.queryDb(findDb(q1, q2))
   result = queryd 
 
 proc getUser*(q1: string, q2: string): seq[JsonNode] = 
   let queryd = userDb.queryDb(findDb(q1, q2))
   result = queryd 
 
-proc saveQuery*(data: JsonNode): bool = 
+proc newUser*(data: JsonNode): bool = 
   dbs.append(data)
   result = true
